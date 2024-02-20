9a61757cd1a22774b8a81aca604f1d324d036b00
skawashi <skawashi.engineer@gmail.com>
2024-02-20 13:15:53 +0900

    ホットリロード対応

--- golang/app/main.go
+++ golang/app/main.go
@@ -43,7 +43,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":"Hello World",
+			"message":"ping",
 		})
 	})
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
